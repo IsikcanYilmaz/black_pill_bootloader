ac6c7fcf88b00a91bb5c1421c76250fd6d0227de
jon <can158@gmail.com>
2020-05-03 00:59:54 -0700

    struggling with dma

--- Core/Src/addressable_led_driver.c
+++ Core/Src/addressable_led_driver.c
@@ -130,11 +130,20 @@ void AddrLED_Init(void)
   LED_PANEL_1_PWM_TIMER->PSC = 0;
   LED_PANEL_1_PWM_TIMER->ARR = 22;
   LED_PANEL_1_PWM_TIMER->EGR = TIM_EGR_UG;
+  
+  //#define PWM_BASE_TEST
+  #define PWM_DMA_TEST
 
-  // PWM Start TEST
+  #ifdef PWM_BASE_TEST
   ADDR_LED_PWM_START();
-
-  ADDR_LED_PWM_SET_DUTY_CYCLE(14);
+  ADDR_LED_PWM_SET_DUTY_CYCLE(22);
+  #endif
+  
+  #ifdef PWM_DMA_TEST
+  const uint32_t dmaTestPayload[] = {1, 1, 22, 22};
+  //HAL_TIM_PWM_Start_DMA(TIM_HandleTypeDef *htim, uint32_t Channel, uint32_t *pData, uint16_t Length)
+  HAL_TIM_PWM_Start_DMA(&LED_PANEL_1_PWM_TIMER_HANDLE, LED_PANEL_1_PWM_TIMER_CHANNEL, &dmaTestPayload, 4);
+  #endif
 
   while(1) { }
 
