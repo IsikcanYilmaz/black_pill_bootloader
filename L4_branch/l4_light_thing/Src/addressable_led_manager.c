9ae00cf7c0b514706e91d511e444a7eabe2c2c50
isikcanYilmaz <can158@gmail.com>
2020-05-25 03:13:32 -0700

    updates

--- L4_branch/l4_light_thing/Src/addressable_led_manager.c
+++ L4_branch/l4_light_thing/Src/addressable_led_manager.c
@@ -11,7 +11,7 @@
 
 // PRIVATE VARIBLES -------------------------------------------------
 
-const uint16_t ledStrip1Size = 32;
+const uint16_t ledStrip1Size = NUM_LEDS_PER_PANEL;
 AddrLEDStrip_t ledStrip1;
 Pixel_t ledStrip1Pixels[sizeof(PixelPacket_t) * 16 * 2];
 uint8_t ledStrip1PacketBuffer[sizeof(PixelPacket_t) * 16 * 2 + 1]; // (3 * 8) * 16 + 1
@@ -28,8 +28,8 @@ static void InitPanel(AddrLEDPanel_t *p)
   for (int i = 0; i < p->numLeds; i++)
   {
     Pixel_t *pixel = &strip[p->stripRange[0] + i];
-    pixel->localX = i / NUM_LEDS_PER_PANEL_SIDE;
-    pixel->localY = i % NUM_LEDS_PER_PANEL_SIDE;
+    pixel->localX = NUM_LEDS_PER_PANEL_SIDE - (i / NUM_LEDS_PER_PANEL_SIDE);
+    pixel->localY = NUM_LEDS_PER_PANEL_SIDE - (i % NUM_LEDS_PER_PANEL_SIDE);
     
     // TODO // HANDLE GLOBAL COORDINATES
   }
@@ -40,7 +40,10 @@ static void InitPanel(AddrLEDPanel_t *p)
 
 static Pixel_t* GetPixelByLocalCoordinate(Position_e pos, uint8_t x, uint8_t y)
 {
-  return NULL;
+  AddrLEDPanel_t *panel = &panels[pos];
+  AddrLEDStrip_t *strip = panel->strip;
+  Pixel_t *pixel = &(strip->pixels[(y * NUM_LEDS_PER_PANEL_SIDE) + x]);
+  return pixel;
 }
 
 static Pixel_t* GetPixelByGlobalCoordinate(uint8_t x, uint8_t y, uint8_t z)
@@ -70,6 +73,20 @@ void AddrLEDManager_Init(void)
   AddrLED_Init(&ledStrip1);
 
   // Initialize the Panel(s)
+  // The LEDs are laid down on the panels like so:
+  //
+  //  0  1  2  3
+  //  7  6  5  4
+  //  8  9 10 11
+  // 15 14 13 12
+  //
+  // We want to map local xy coordinates to LEDs like so:
+  //
+  // 12 13 14 15
+  //  8  9 10 11
+  //  4  5  6  7
+  //  0  1  2  3
+  //
   for (int panelIdx = 0; panelIdx < NUM_PANELS; panelIdx++)
   {
     Position_e pos = (Position_e) panelIdx;
@@ -83,6 +100,25 @@ void AddrLEDManager_Init(void)
     InitPanel(&p);
     panels[pos] = p;
 
+    // Initialize LEDs in the panel
+    for (int ledIdx = p.stripRange[0]; ledIdx <= p.stripRange[1]; ledIdx++)
+    {
+  //  0  1  2  3
+  //  7  6  5  4
+  //  8  9 10 11
+  // 15 14 13 12
+      Pixel_t *led = (Pixel_t *) &(p.strip->pixels[ledIdx]);
+      if (ledIdx < 4 || (ledIdx > 7 && ledIdx < 12))
+      {
+        
+      }
+      else
+      {
+
+      }
+
+    }
+
     // TODO // Remove below when you get the full cube
     break;
   }
@@ -93,29 +129,38 @@ void AddrLEDManager_SanityTest(void)
   bool toggle = false;
   uint8_t c = 1;
   bool addc = true;
-  uint8_t top = 100;
+  uint8_t top = 10;
   uint8_t stage = 0;
   while(1){
 
     TOGGLE_ONBOARD_LED();
 
+#define TEST 0
+#if (TEST == 0)   // CURRENT DRAW TEST
     for (int i = 0; i < ledStrip1.numLeds; i++)
     {
       Pixel_t color1, color2;
-#define CURRENT_TEST 0
-#if CURRENT_TEST
       uint8_t r = 1;
-      uint8_t g = 73;
+      uint8_t g = 10;
       uint8_t b = 1;
       color1 = (Pixel_t) {.red = r, .green = g, .blue = b};
       Pixel_t *currPixel = &(ledStrip1.pixels[i]);
       *currPixel = color1;
-#else
+    }
+
+    Pixel_t *p1 = GetPixelByLocalCoordinate(NORTH, 0, 0);
+    Pixel_t co = {0, 10, 10};
+    *p1 = co;
+
+#elif (TEST == 1) // REGULAR TEST
+    for (int i = 0; i < ledStrip1.numLeds; i++)
+    {
+      Pixel_t color1, color2;
       switch(stage)
       {
         case 0:
-          color1 = (Pixel_t) {.red = c, .green = c, .blue = top-c};
-          color2 = (Pixel_t) {.red = top-c, .green = top-c, .blue = c};
+          color1 = (Pixel_t) {.red = c, .green = 0, .blue = top-c};
+          color2 = (Pixel_t) {.red = top-c, .green = 0, .blue = c};
           break;
 
         case 1:
@@ -139,16 +184,22 @@ void AddrLEDManager_SanityTest(void)
       {
         *currPixel = color2;
       }
+    }
+
+#elif (TEST == 2) // ADDRESSING TEST
+    volatile Pixel_t *p = GetPixelByLocalCoordinate(NORTH, 1, 1);
+    
+    Pixel_t color1 = {10, 0, 0};
+    *p = color1;
 #endif
 
       //if (i == 15)
       //  break;
-    }
     //toggle = !toggle;
 
     AddrLED_SanityTest(&ledStrip1);
     //IDLE_FOREVER(100);
-    HAL_Delay(10);
+    HAL_Delay(100);
 
     // We need to stop the pwm timer after our payload is sent and start it back up again
     AddrLED_StopPWM();
