3aa2af003ffd6202817173f1b8c74f9378284a23
isikcanYilmaz <can158@gmail.com>
2020-06-20 13:30:29 -0700

    pixel bench color test animation

---
+++ tools/pixel_testbench/color_test_animation.py
@@ -0,0 +1,73 @@
+
+from classes import *
+from common import *
+
+ANIMATION_UPDATE_FREQUENCY = 1
+ANIMATION_UPDATE_PERIOD_MS = int(100 / ANIMATION_UPDATE_FREQUENCY)
+
+class ColorTestMetaData:
+    def __init__(self):
+        self.offTime = 0
+        self.decrementRate = 1
+
+class ColorTest:
+    def __init__(self, panelsPtr):
+        self.r = self.g = self.b = 0
+
+        self.h = 0
+        self.s = 0
+        self.v = 100
+
+        self.addv = True
+        self.addh = True
+        self.adds = True
+
+        self.ratev = 0
+        self.rateh = 1
+        self.rates = 1
+
+        self.panelsPtr = panelsPtr
+        self.panelMetadata = []
+        for p in range(0, NUM_PANELS):
+            panelArr = []
+            for y in range(0, PANEL_PIXEL_WIDTH):
+                row = []
+                for x in range(0, PANEL_PIXEL_WIDTH):
+                    m = ColorTestMetaData()
+                    m.offTime = randint(0, 1000)
+                    row.append(m)
+                panelArr.append(row)
+            self.panelMetadata.append(panelArr)
+
+    def start(self):
+        pygame.time.set_timer(UPDATE_EVENT, ANIMATION_UPDATE_PERIOD_MS)
+
+    def update(self):
+        for panelIdx, panel in enumerate(self.panelsPtr):
+            for x in range(0, PANEL_PIXEL_WIDTH):
+                for y in range(0, PANEL_PIXEL_WIDTH):
+                    pix = panel.getPixelByCoords(x, y)
+                    (rr, gg, bb) = hls_to_rgb(self.h / 359, self.s / 100,  self.v / 100)
+                    (pix.r, pix.g, pix.b) = (int(rr * 255), int(gg * 255), int(bb * 255))
+        print(pix)
+
+        if (self.addv):
+            self.v += self.ratev
+        else:
+            self.v -= self.ratev
+        if (self.v >= 100 or self.v <= 0):
+            self.addv = not self.addv
+
+        if (self.addh):
+            self.h += self.rateh
+        else:
+            self.h -= self.rateh
+        if (self.h >= 359 or self.h <= 0):
+            self.addh = not self.addh
+
+        if (self.adds):
+            self.s += self.rates
+        else:
+            self.s -= self.rates
+        if (self.s >= 100 or self.s <= 0):
+            self.adds = not self.adds
