248587bdff46b95ef980f8f9e9e540600ae97079
isikcanYilmaz <can158@gmail.com>
2020-12-25 01:13:05 -0800

    board works programs, prints on cDc

--- Src/main.c
+++ Src/main.c
@@ -94,22 +94,22 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
-  MX_USART2_UART_Init();
-  MX_ADC1_Init();
-  MX_I2C1_Init();
-  MX_I2C2_Init();
-  MX_UART4_Init();
-  MX_USB_PCD_Init();
+  /*MX_GPIO_Init();*/
+  /*MX_DMA_Init();*/
+  /*MX_TIM2_Init();*/
+  /*MX_TIM3_Init();*/
+  /*MX_USART2_UART_Init();*/
+  /*MX_ADC1_Init();*/
+  /*MX_I2C1_Init();*/
+  /*MX_I2C2_Init();*/
+  /*MX_UART4_Init();*/
+  /*MX_USB_PCD_Init();*/
   /* USER CODE BEGIN 2 */
   
-  ButtonDriver_Init();
-  DbgUart_Init();
-  AddrLEDManager_Init();
-  Geometry_Init();
+  /*ButtonDriver_Init();*/
+  /*DbgUart_Init();*/
+  /*AddrLEDManager_Init();*/
+  /*Geometry_Init();*/
 
   // Infinite loop v // TODO // refactor this test stuff
   AddrLEDManager_SanityTest();
---
+++ board_bringup/Inc/main.h
@@ -0,0 +1,87 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define GREEN_LED_Pin GPIO_PIN_5
+#define GREEN_LED_GPIO_Port GPIOA
+#define BATT_ADC_Pin GPIO_PIN_6
+#define BATT_ADC_GPIO_Port GPIOA
+#define MIC_IN_ADC_Pin GPIO_PIN_0
+#define MIC_IN_ADC_GPIO_Port GPIOB
+#define MIC_EN_Pin GPIO_PIN_2
+#define MIC_EN_GPIO_Port GPIOB
+#define CHARGER_STAT1_Pin GPIO_PIN_14
+#define CHARGER_STAT1_GPIO_Port GPIOB
+#define CHARGER_STAT2_Pin GPIO_PIN_15
+#define CHARGER_STAT2_GPIO_Port GPIOB
+#define BATT_ADC_EN_Pin GPIO_PIN_6
+#define BATT_ADC_EN_GPIO_Port GPIOC
+#define BOOST_EN_Pin GPIO_PIN_7
+#define BOOST_EN_GPIO_Port GPIOC
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
