d33b55f1854b771d83204f03faf5b2737fc48ae1
Isikcan Yilmaz <isikcanyilmaz@apple.com>
2022-08-16 21:34:20 +0300

    updates

--- Inc/dbg_uart.h
+++ Inc/dbg_uart.h
@@ -2,8 +2,8 @@
 #define CNFG_DBG_UART_RX_BUFFER_SIZE 256
 #define CNFG_DBG_UART_TX_BUFFER_SIZE 256
 
-#define DBG_UART_INSTANCE UART4
-#define DBG_UART_HANDLE   huart4
+#define DBG_UART_INSTANCE UART2
+#define DBG_UART_HANDLE   huart2
 
 void DbgUart_Init(void);
 void DbgUart_Isr(void);
